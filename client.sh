a91d6b7c8e3c0001d9ef3cc6c6ee2fe5dda0be96
Kohei Noda <k.noda@saver.jp>
2026-01-09 15:35:50 +0900

    client.sh: add DEVICE env to use mic instead of audiotestsrc

--- client.sh
+++ client.sh
@@ -7,10 +7,20 @@ VOLUME=0.2
 BITRATE=64000
 HOST=127.0.0.1
 PORT=5000
+DEVICE=${DEVICE:-}
 
-gst-launch-1.0 -v audiotestsrc is-live=true volume=$VOLUME wave=$WAVE \
-    ! audioconvert \
-    ! audioresample \
-    ! opusenc bitrate=$BITRATE \
-    ! rtpopuspay pt=96 \
-    ! udpsink host=$HOST port=$PORT
+if [ "${DEVICE:-}" == "" ]; then
+    gst-launch-1.0 -v audiotestsrc is-live=true volume=$VOLUME wave=$WAVE \
+        ! audioconvert \
+        ! audioresample \
+        ! opusenc bitrate=$BITRATE \
+        ! rtpopuspay pt=96 \
+        ! udpsink host=$HOST port=$PORT
+else
+    gst-launch-1.0 -v pulsesrc device="$DEVICE" \
+        ! audioconvert \
+        ! audioresample \
+        ! opusenc bitrate=$BITRATE \
+        ! rtpopuspay pt=96 \
+        ! udpsink host=$HOST port=$PORT
+fi
